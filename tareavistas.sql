298a540d95d2207a30ae2c70e56864dbd047119a
kevingo710 <kevin.gracia.tic@gmail.com>
2020-06-11 15:17:20 -0500

    Im forget push view 2 spotifyBD

--- tareavistas.sql
+++ tareavistas.sql
@@ -8,6 +8,21 @@ SELECT * FROM VISTA_SESIONES;
 
 --Vista 2 Crear una tabla songs_play que tenga id_song e id_usuario e veces reproducidas
 
+create definer = root@`%` view VISTA_MOSTPLAYED as
+select `spotify`.`SONGS`.`TITLE`                          AS `SONG`,
+       `spotify`.`ALBUM`.`TITLE`                          AS `ALBUM`,
+       `spotify`.`ARTIST`.`NAME`                          AS `ARTIST`,
+       count(`spotify`.`PLAYED_SONGS`.`SONG_ID`)          AS `PLAYEDS_COUNT`,
+       count(distinct `spotify`.`PLAYED_SONGS`.`USER_ID`) AS `PLAYEDS_DIFF_USERS`
+from ((((`spotify`.`SONGS` join `spotify`.`ALBUM`) join `spotify`.`USER`) join `spotify`.`ARTIST`)
+         join `spotify`.`PLAYED_SONGS`)
+where ((`spotify`.`USER`.`ID` = `spotify`.`PLAYED_SONGS`.`USER_ID`) and
+       (`spotify`.`ARTIST`.`ID` = `spotify`.`ALBUM`.`ARTIST_ID`) and
+       (`spotify`.`ALBUM`.`ID` = `spotify`.`SONGS`.`ALBUM_ID`) and
+       (`spotify`.`SONGS`.`ID` = `spotify`.`PLAYED_SONGS`.`SONG_ID`))
+group by `spotify`.`PLAYED_SONGS`.`SONG_ID`
+order by `PLAYEDS_COUNT` desc;
+
 --Vista 3 Usuario login
 
 CREATE VIEW VISTA_USUARIOS AS
