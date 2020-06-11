a1a193d29a0119704a9042e270cf6bc527df2cf9
kevingo710 <kevin.gracia.tic@gmail.com>
2020-06-11 15:21:07 -0500

    VISTA  2 BEST CLIENT

--- classicViewsP.sql
+++ classicViewsP.sql
@@ -9,3 +9,17 @@ GROUP BY products.productCode
 ORDER BY SUM(quantityOrdered) DESC
 LIMIT 1;
 	
+
+--Crear una vista que visualiza cual ha sido el cliente con mayor cantidad de compras (Parametrizar por rango de fecha)
+--(Nombre del cliente, Venta total por cliente, numero de diferentes productos que ha adquirido el cliente)
+
+
+CREATE VIEW BESTCLIENT AS
+SELECT customers.customerName AS nombreCliente,SUM(orderdetails.quantityOrdered * orderdetails.priceEach) as ventaTotalCliente,
+       COUNT(DISTINCT (orderdetails.productCode)) AS productosDiferentes, MIN(orders.orderDate) AS fechaInicio, MAX(orders.orderDate )as fechaFin
+FROM customers, orderdetails, orders
+WHERE customers.customerNumber = orders.customerNumber AND orders.orderNumber = orderdetails.orderNumber
+GROUP BY customers.customerName
+ORDER BY productosDiferentes DESC, fechaInicio, fechaFin
+LIMIT 1;
+
